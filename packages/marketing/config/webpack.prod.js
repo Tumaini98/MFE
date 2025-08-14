5da95a3eae27bf6299b459cb63a05d7bec31cf68
Joshua Ukiru <103338142+Tumaini98@users.noreply.github.com>
2025-08-14 11:40:52 -0700

    Fix webpack and CI configuration

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -2,8 +2,6 @@ const { merge } = require("webpack-merge");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
 const commonConfig = require("./webpack.common");
-const { plugins } = require("../../container/config/webpack.common");
-
 const prodConfig = {
   mode: "production",
   output: {
